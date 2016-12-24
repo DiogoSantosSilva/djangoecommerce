46465a76090bccf95faf1f68c5f0751e615bc1ac
diogo <diogo@localhost.localdomain>
2016-12-24 13:23:32 -0200

    procfile

--- ecommerce2/settings/local.py
+++ ecommerce2/settings/local.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #root of project
@@ -133,6 +134,10 @@ STATICFILES_DIRS = (
     #os.path.join(BASE_DIR, "static_in_env"),
     #'/var/www/static/',
 )
+
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
