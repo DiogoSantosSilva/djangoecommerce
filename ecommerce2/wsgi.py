30b85b0ed766b5eb2045a7832e6730a04682e777
diogo <diogo@localhost.localdomain>
2017-02-07 21:53:51 -0200

    error

--- ecommerce2/wsgi.py
+++ ecommerce2/wsgi.py
@@ -1,20 +1,16 @@
 """
-WSGI config for ecommerce2 project.
+WSGI config for projectweb project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings")
 
 application = get_wsgi_application()
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
