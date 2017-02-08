a8adad799b403a3983f46d66f684d3317c2be678
diogo <diogo@localhost.localdomain>
2017-02-08 12:15:29 -0200

    updating django

--- ecommerce2/settings/base.py
+++ ecommerce2/settings/base.py
@@ -128,16 +128,16 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "static_root")
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname((BASE_DIR), "static"))
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
     #os.path.join(BASE_DIR, "static_in_env"),
     #'/var/www/static/',
 )
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media")
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR), "media"))
 
 
 
