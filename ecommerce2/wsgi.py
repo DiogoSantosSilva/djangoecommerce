283e79a0c9d877519f7085c8babff5b296949f4a
diogo <diogo@localhost.localdomain>
2017-02-08 13:29:46 -0200

    updating django

--- ecommerce2/wsgi.py
+++ ecommerce2/wsgi.py
@@ -12,4 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-ecommerce2 = Cling(get_wsgi_application())
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings") #nirla is the name of the project
+
+
+application = Cling(get_wsgi_application())
