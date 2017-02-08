afc8e50b68c48e5cec3b4cc39a6eeec3466bc2dd
diogo <diogo@localhost.localdomain>
2017-02-08 00:00:43 -0200

    error

--- ecommerce2/urls.py
+++ ecommerce2/urls.py
@@ -33,8 +33,6 @@ urlpatterns = [
     url(r'^seller/', include('sellers.urls', namespace='sellers')),
 ]
 
-
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
