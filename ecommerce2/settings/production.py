5b8f050ca4ef395585abddfb030d7d026b0a74b3
diogo <diogo@localhost.localdomain>
2017-02-08 14:27:16 -0200

    staticfiles

--- ecommerce2/settings/production.py
+++ ecommerce2/settings/production.py
@@ -123,13 +123,22 @@ if not settings.DEBUG:
 	#production deploy postgres
 
 	# keep this
-	DATABASES = {
-	    'default': {
-	        'ENGINE': 'django.db.backends.sqlite3',
-	        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-	    }
-	}
+	if ON_HEROKU == '1':
 
+	    # Parse database configuration from $DATABASE_URL
+	    import dj_database_url
+	#    DATABASES['default'] = dj_database_url.config()
+	    DATABASES = {'default' : dj_database_url.config()}
+
+
+	else:
+	    # else we use local sqlite database
+	    DATABASES = {
+	        'default': {
+	            'ENGINE': 'django.db.backends.sqlite3',
+	            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	        }
+	    }
 	# add this
 	import dj_database_url
 	db_from_env = dj_database_url.config()
