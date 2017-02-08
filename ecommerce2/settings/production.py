a3bcaabc26d53cac315b94ee90d42cc33c239e9b
diogo <diogo@localhost.localdomain>
2017-02-08 01:37:27 -0200

    gitignore

--- ecommerce2/settings/production.py
+++ ecommerce2/settings/production.py
@@ -24,6 +24,21 @@ from django.conf import settings
 if not settings.DEBUG:
 	import os
 
+		in_heroku = False
+	if 'DATABASE_URL' in os.environ:
+	    in_heroku = True
+
+	import dj_database_url
+	if in_heroku:
+	    DATABASES = {'default': dj_database_url.config()}
+	else:
+	    DATABASES = {
+	        'default': {
+	            'ENGINE': 'django.db.backends.sqlite3',
+	            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+	        }
+	    }
+
 	BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 	#root of project
 
