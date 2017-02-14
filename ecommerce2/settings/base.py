139e9bbe4e7e63bf7988e76e87b50a2d0d854fc8
diogo <diogo@localhost.localdomain>
2017-02-13 23:55:34 -0200

    new css

--- ecommerce2/settings/base.py
+++ ecommerce2/settings/base.py
@@ -150,32 +150,3 @@ BRAINTREE_PUBLIC = "6rhmcym3ppvggbrq"
 BRAINTREE_PRIVATE = "db78285aa54368a5c299ccf62f0f0530"
 BRAINTREE_MERCHAND_ID = "kkj4x5y4nq5msn5r"
 BRAINTREE_ENVIRONMENT = "sandbox"
-
-
-AWS_ACCESS_KEY_ID = "AKIAJBXGEKRT5MG4ML2A"
-AWS_SECRET_ACCESS_KEY = "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"
-
-AWS_FILE_EXPIRE = 200
-AWS_PRELOAD_METADATA = True
-AWS_QUERYSTRING_AUTH = True
-
-DEFAULT_FILE_STORAGE = 'ecommerce2.utils.MediaRootS3BotoStorage'
-STATICFILES_STORAGE = 'ecommerce2.utils.StaticRootS3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'multi-ecommerce'
-S3DIRECT_REGION = 'us-west-2'
-S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-MEDIA_ROOT = MEDIA_URL
-STATIC_URL = S3_URL + 'static/'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-
-import datetime
-
-two_months = datetime.timedelta(days=61)
-date_two_months_later = datetime.date.today() + two_months
-expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
-
-AWS_HEADERS = {
-    'Expires': expires,
-    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
-}
