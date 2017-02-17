6ca8d462f5c02be80f36f9c9d4bd8a92da775918
diogo <diogo@localhost.localdomain>
2017-02-17 10:52:54 -0200

    error

--- ecommerce2/settings/production.py
+++ ecommerce2/settings/production.py
@@ -68,6 +68,18 @@ DATABASES = {
     }
 }
 
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+
 # add this
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
