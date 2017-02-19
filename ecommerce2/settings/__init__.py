addb6ae42ab4e7d09ac17857790f06581f2add0c
diogo <diogo@localhost.localdomain>
2017-02-18 23:27:35 -0300

    new style

--- ecommerce2/settings/__init__.py
+++ ecommerce2/settings/__init__.py
@@ -1,14 +1,14 @@
+
 from .base import *
 
-if base.DEBUG:
-	try:
-		from .local import *
-	except:
-		pass
-elif not base.DEBUG:
-	try:
-		from .production import *
-	except:
-		pass
-else:
+
+try:
+	from .local import *
+except:
+	pass
+
+
+try:
+	from .production import *
+except:
 	pass
