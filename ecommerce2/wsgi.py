fcbab4226bef9de2c8cd8526255109bb4a271836
diogo <diogo@localhost.localdomain>
2017-02-19 00:02:12 -0300

    new style

--- ecommerce2/wsgi.py
+++ ecommerce2/wsgi.py
@@ -10,7 +10,13 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings") #ecommerce2 is the name of the project
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecommerce2.settings") #ecommerce2 is the name of the project
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+
+
+
+
+application = Cling(get_wsgi_application())
