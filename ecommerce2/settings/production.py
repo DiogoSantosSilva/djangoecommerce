f8b3d3523d534a8b864d67bb81a28a966a4853cc
diogo <diogo@localhost.localdomain>
2017-02-20 00:17:21 -0300

    django registration issue

--- ecommerce2/settings/production.py
+++ ecommerce2/settings/production.py
@@ -116,11 +116,6 @@ STATICFILES_DIRS = (
     #'/var/www/static/',
 )
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-#os.path.join(os.path.dirname(BASE_DIR), "static_in_env", "media_root")
-
-
 #Crispy FORM TAGs SETTINGS
 CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
