8254b2cb5e67928ab2a3e3cef2fa4def2404d86d
diogo <diogo@localhost.localdomain>
2017-02-21 13:22:58 -0300

    errors

--- products/migrations/0001_initial.py
+++ products/migrations/0001_initial.py
@@ -16,10 +16,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Category',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=120)),
                 ('slug', models.SlugField(unique=True)),
-                ('description', models.TextField(null=True, blank=True)),
+                ('description', models.TextField(blank=True, null=True)),
                 ('active', models.BooleanField(default=True)),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
             ],
@@ -27,23 +27,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Product',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=120)),
-                ('description', models.TextField(null=True, blank=True)),
+                ('description', models.TextField(blank=True, null=True)),
                 ('price', models.DecimalField(max_digits=20, decimal_places=2)),
                 ('active', models.BooleanField(default=True)),
-                ('categories', models.ManyToManyField(to='products.Category', blank=True)),
-                ('default', models.ForeignKey(related_name='default_category', blank=True, to='products.Category', null=True)),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.CreateModel(
             name='ProductFeatured',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('image', models.ImageField(upload_to=products.models.image_upload_to_featured)),
-                ('title', models.CharField(max_length=120, null=True, blank=True)),
-                ('description', models.CharField(max_length=220, null=True, blank=True)),
+                ('title', models.CharField(blank=True, max_length=120, null=True)),
+                ('description', models.CharField(blank=True, max_length=220, null=True)),
                 ('make_image_background', models.BooleanField(default=False)),
                 ('text_rigth', models.BooleanField(default=False)),
                 ('show_price', models.BooleanField(default=False)),
@@ -54,21 +51,52 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='ProductImage',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('image', models.ImageField(upload_to=products.models.image_upload_to)),
                 ('product', models.ForeignKey(to='products.Product')),
             ],
         ),
         migrations.CreateModel(
+            name='SubCategory',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('title', models.CharField(max_length=120)),
+                ('slug', models.SlugField(unique=True)),
+                ('active', models.BooleanField(default=True)),
+                ('timestamp', models.DateTimeField(auto_now_add=True)),
+                ('Category', models.ForeignKey(to='products.Category', related_name='category')),
+            ],
+        ),
+        migrations.CreateModel(
             name='Variation',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=120)),
                 ('price', models.DecimalField(max_digits=20, decimal_places=2)),
-                ('sale_price', models.DecimalField(null=True, max_digits=20, decimal_places=2)),
+                ('sale_price', models.DecimalField(max_digits=20, decimal_places=2, null=True)),
                 ('active', models.BooleanField(default=True)),
-                ('inventory', models.IntegerField(default=b'-1', null=True, blank=True)),
+                ('inventory', models.IntegerField(default='-1', blank=True, null=True)),
                 ('product', models.ForeignKey(to='products.Product')),
             ],
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='Subcategory',
+            field=models.ForeignKey(to='products.SubCategory', related_name='default_subcategory'),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='categories',
+            field=models.ManyToManyField(to='products.Category', blank=True),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='default',
+            field=models.ForeignKey(to='products.Category', blank=True, related_name='default_category', null=True),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='user',
+            field=models.ForeignKey(to=settings.AUTH_USER_MODEL),
         ),
     ]
